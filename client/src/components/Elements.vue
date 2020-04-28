e848c3c3fd70d81745d7f5cc14f349b46ad298bb
Leo <lkachatkou@kaliop.com>
2020-04-28 16:16:31 +0200

    login view done

---
+++ client/src/components/Elements.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1 class="header-1">Header 1</h1>
+        <h2 class="header-2">Header 2</h2>
+        <h3 class="header-3">Header 3</h3>
+        <a href="#" class="link">link</a>
+        <br>
+        <button class="project-btn">Sign in</button>
+        <button class="project-btn project-btn--red">Delete</button>
+        <form>
+            <div class="form-label"><label for="fname">First name:</label></div>
+            <input type="text" id="fname" name="fname" class="form-input">
+            <br>
+            <label for="select" class="sr-only">First name:</label>
+            <select id="select" class="form-input form-select">
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="mercedes">Mercedes</option>
+                <option value="audi">Audi</option>
+            </select>
+            <br>
+            <label for="textarea" class="sr-only">First name:</label>
+            <textarea id="w3mission" rows="4" cols="50" class="form-input form-textarea">
+                At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies.
+            </textarea>
+        </form>
+        <div class="tag">
+            <div>Lol</div>
+            <div class="tag__close"><img src="../img/icons/close.svg" alt=""></div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Elements',
+    props: {
+        msg: String
+    }
+}
+</script>
+
---
+++ client/src/components/Login.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="login">
+        <div class="login__form-ctn">
+            <div class="login__form">
+                <div class="login__title">
+                    <h1 class="header-1">Log in</h1>
+                </div>
+                <form>
+                    <div class="login__form-input">
+                        <img src="../img/icons/person.svg" alt="">
+                        <label for="email" class="sr-only">Email</label>
+                        <input type="email" id="email" class="form-input" placeholder="Email">
+                    </div>
+                    <div class="login__form-input">
+                        <img src="../img/icons/lock.svg" alt="">
+                        <label for="password" class="sr-only">Email</label>
+                        <input type="password" id="password" class="form-input" placeholder="Password">
+                    </div>
+                    <div class="login__submit-btn-ctn">
+                        <button class="project-btn" type="submit">Sign in</button>
+                    </div>
+                </form>
+                <div class="login__actions">
+                    <div>New account? <a href="#" class="link">Register</a></div>
+                    <div><a href="#" class="link">Forgot password?</a></div>
+                </div>
+            </div>
+        </div>
+        <div class="login__img-ctn">
+            <img src="../img/loginBG.svg" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
